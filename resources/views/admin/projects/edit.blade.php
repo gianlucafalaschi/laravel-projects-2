b12ed6bfc248036c4e22ace48e446d9439d549cc
gianlucafalaschi <gian100gian@outlook.it>
2024-06-24 22:53:47 +0200

    old in edit form

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -21,17 +21,18 @@
 
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" value="{{ $project->name }}">
+        {{-- se c'e' l'old stampa l'old, altrimenti stampa il $project->name del database --}}
+        <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $project->name) }}"> 
     </div>
 
     <div class="mb-3">
         <label for="client_name" class="form-label">Client name</label>
-        <input type="text" class="form-control" id="client_name" name="client_name" value="{{ $project->client_name }}">
+        <input type="text" class="form-control" id="client_name" name="client_name" value="{{ old('client_name', $project->client_name) }}">
     </div>
       
     <div class="mb-3">
         <label for="summary" class="form-label">Summary</label>
-        <textarea class="form-control" id="summary" name="summary" rows="8">{{ $project->summary }}</textarea>
+        <textarea class="form-control" id="summary" name="summary" rows="8">{{ old('summary', $project->summary) }}</textarea>
     </div>
     
     <button type="submit" class="btn btn-primary">Save</button>
